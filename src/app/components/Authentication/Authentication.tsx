0d8baf17e883fa703313c4e37c54d6eec0cd0d94
tourniquet <admyn3d@gmail.com>
2023-09-21 20:56:35 +0100

    Authentication component

---
+++ src/app/components/Authentication/Authentication.tsx
@@ -0,0 +1,62 @@
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import { useContext } from 'react'
+import { Avatar, Button } from 'antd'
+import styled from 'styled-components'
+
+import { auth } from '../../../../firebase-config'
+import { TodoContext } from '../../../contexts/TodoContext'
+
+const AvatarStyled = styled(Avatar)`
+  @media (min-width: 970px) {
+    float: right;
+    margin-right: 465px;
+  }
+`
+const ButtonStyled = styled(Button)`
+  @media (min-width: 970px) {
+    margin-left: 465px;
+  }
+`
+
+export default function Authentication (): JSX.Element {
+  const { user }: { user?: { photoURL: string } } = useContext(TodoContext) // TODO: Find right types, not ANY
+
+  const googleProvider = new GoogleAuthProvider()
+  const googleLogin = async (): Promise<void> => {
+    try {
+      await signInWithPopup(auth, googleProvider)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  function logOut (): void {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        console.log('Sign-out successful')
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log('An error happened', error)
+      })
+  }
+
+  return (
+    <>
+      {user !== null
+        ? (
+          <>
+            <ButtonStyled type='primary' onClick={logOut}>Log out</ButtonStyled>
+            <AvatarStyled size='large' src={user?.photoURL} />
+          </>
+          )
+        : (
+          <>
+            <ButtonStyled onClick={() => { void googleLogin() }}>Login</ButtonStyled>
+            <AvatarStyled size='large' />
+          </>
+          )}
+    </>
+  )
+}

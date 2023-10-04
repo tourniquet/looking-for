d9417d07d2d52bfdf3303a13bf73ed5d7d764963
tourniquet <admyn3d@gmail.com>
2023-10-04 20:52:11 +0100

    All components on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
 
-export default function Home() {
+import { Col, Row } from 'antd'
+
+import { ItemProvider } from '../contexts/ItemContext'
+
+import NavBar from './components/NavBar/NavBar'
+import NewTodo from './components/NewItem/NewItem'
+import UnorderedList from './components/UnorderedList/UnorderedList'
+import { MantineProvider } from '@mantine/core'
+
+export default function Home (): JSX.Element {
   return (
-    <main className={styles.main}>
-    </main>
+    <MantineProvider>
+      <ItemProvider>
+        <NavBar />
+
+        <Row>
+          <Col xs={{ span: 24 }} sm={{ span: 24 }} lg={{ span: 12, offset: 6 }}>
+            <NewTodo />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs={{ span: 24 }} sm={{ span: 24 }} lg={{ span: 12, offset: 6 }}>
+            <UnorderedList />
+          </Col>
+        </Row>
+
+      </ItemProvider>
+    </MantineProvider>
   )
 }

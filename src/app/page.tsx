b0a411b1304683003e4aebab4264bb32e8a15b9f
tourniquet <admyn3d@gmail.com>
2023-11-18 23:18:25 +0000

    removed Mantine

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,27 +7,23 @@ import { ItemProvider } from '../contexts/ItemContext'
 import NavBar from './components/NavBar/NavBar'
 import NewTodo from './components/NewItem/NewItem'
 import UnorderedList from './components/UnorderedList/UnorderedList'
-import { MantineProvider } from '@mantine/core'
 
 export default function Home (): JSX.Element {
   return (
-    <MantineProvider>
-      <ItemProvider>
-        <NavBar />
+    <ItemProvider>
+      <NavBar />
 
-        <Row>
-          <Col xs={{ span: 24 }} sm={{ span: 24 }} lg={{ span: 12, offset: 6 }}>
-            <NewTodo />
-          </Col>
-        </Row>
+      <Row>
+        <Col xs={{ span: 24 }} sm={{ span: 24 }} lg={{ span: 12, offset: 6 }}>
+          <NewTodo />
+        </Col>
+      </Row>
 
-        <Row>
-          <Col xs={{ span: 24 }} sm={{ span: 24 }} lg={{ span: 12, offset: 6 }}>
-            <UnorderedList />
-          </Col>
-        </Row>
-
-      </ItemProvider>
-    </MantineProvider>
+      <Row>
+        <Col xs={{ span: 24 }} sm={{ span: 24 }} lg={{ span: 12, offset: 6 }}>
+          <UnorderedList />
+        </Col>
+      </Row>
+    </ItemProvider>
   )
 }

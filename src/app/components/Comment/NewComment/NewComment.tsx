148a7e187fd1e180bcd8881e52b180cce2a0c954
tourniquet <admyn3d@gmail.com>
2023-12-28 18:39:25 +0000

    NewComment styles

--- src/app/components/Comment/NewComment/NewComment.tsx
+++ src/app/components/Comment/NewComment/NewComment.tsx
@@ -1,9 +1,22 @@
 import { arrayUnion, doc, updateDoc } from 'firebase/firestore'
 import { Button, Input } from 'antd'
 import { useContext } from 'react'
+import styled from 'styled-components'
 
 import { ItemContext, ItemContextType } from '@/contexts/ItemContext'
 import { db } from '../../../../../firebase-config'
+
+const NewCommentBlock = styled.div`
+  display: flex;
+  width: 100%;
+`
+
+const InputStyled = styled(Input)`
+  border-start-start-radius: 6px !important; // TODO: Remove !important
+  border-end-start-radius: 6px !important; // TODO: Remove !important
+  border-bottom-right-radius: 0;
+  border-top-right-radius: 0;
+`
 
 export default function NewComment ({ id }: { id: string }): JSX.Element {
   const { user, comment, setComment, getItems } = useContext(ItemContext) as ItemContextType
@@ -19,8 +32,8 @@ export default function NewComment ({ id }: { id: string }): JSX.Element {
   }
 
   return (
-    <div style={{ display: 'flex', width: '100%' }}>
-      <Input
+    <NewCommentBlock>
+      <InputStyled
         disabled={user === null}
         value={comment}
         itemID={id}
@@ -34,6 +47,6 @@ export default function NewComment ({ id }: { id: string }): JSX.Element {
       >
         Submit
       </Button>
-    </div>
+    </NewCommentBlock>
   )
 }

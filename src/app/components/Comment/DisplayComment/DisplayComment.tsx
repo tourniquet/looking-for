5f7133711e1f1fffd20a21f898b88de518fb70f5
tourniquet <admyn3d@gmail.com>
2023-12-28 18:43:10 +0000

    DisplayComments styles

--- src/app/components/Comment/DisplayComment/DisplayComment.tsx
+++ src/app/components/Comment/DisplayComment/DisplayComment.tsx
@@ -1,13 +1,30 @@
 import { ItemsCollection } from '@/contexts/ItemContext'
+import styled from 'styled-components'
+
+const ULStyled = styled.ul`
+  list-style-type: none;
+  width: 100%;
+`
+
+const LItemStyled = styled.li`
+  border-bottom: 1px solid #d9d9d9; // TODO: Temporary styles
+  min-height: 30px;
+  /* border-bottom: 0; */
+  padding: 4px;
+
+  &:last-child {
+    border-bottom: 0;
+  }
+`
 
 export default function DisplayComment ({ itemObj }: { itemObj: ItemsCollection }): JSX.Element {
   return (
-    <div className='comments'>
+    <ULStyled className='comments'>
       {itemObj.comments.length > 0 && itemObj.comments.map((comment, index) => (
-        <div key={index}>
+        <LItemStyled key={index}>
           <p>{comment.comment}</p>
-        </div>
+        </LItemStyled>
       ))}
-    </div>
+    </ULStyled>
   )
 }
